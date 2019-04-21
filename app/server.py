497d479293b67a53a1787771764eab52d588e198
Hera-Arif <37600323+Hera-Arif@users.noreply.github.com>
2019-04-21 22:57:03 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=1Uog9ESLlgmW0A0iU00_AK87-LUtndClt'
-export_file_name = 'cnn_model.json'
+export_file_name = 'export.json'
 
 classes = ['EarlyBlight', 'LateBlight', 'Healthy']
 path = Path(__file__).parent
